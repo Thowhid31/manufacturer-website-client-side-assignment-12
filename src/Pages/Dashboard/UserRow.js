b45c1a89325141b059a64fee9f6f13fe447d9827
Thowhid31 <thowhidantor16@gmail.com>
2022-05-24 12:39:44 +0600

    implement security for admin access

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -5,17 +5,23 @@ const UserRow = ({ user, index, refetch }) => {
     const { email, role } = user
 
     const makeAdmin = () => {
-        fetch(`http://localhost:5000/user/admin/${email}`,{
+        fetch(`http://localhost:5000/user/admin/${email}`, {
             method: 'PUT',
             headers: {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-        .then(res => res.json())
-        .then(data => {
-            refetch()
-            toast.success('You add an user as an Admin')
-        })
+            .then(res => { 
+                if(res.status ===403){
+                    toast.error('Failed to Make an Admin')
+                }
+                return res.json() })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch()
+                    toast.success('You add an user as an Admin')
+                }
+            })
     }
 
     return (
