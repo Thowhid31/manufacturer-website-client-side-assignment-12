7fca2bae6a7210d2bd50defc5a06d1033ee74283
Thowhid31 <thowhidantor16@gmail.com>
2022-05-25 21:59:32 +0600

    added my orders on dashbord

--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -1,19 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
-const MyOrder = () => {
+const MyOrders = () => {
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
 
-
-    const [orders, setOrders] = useState([])
-
-    // useEffect(()=>{
-
-    // },[])
-
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/orders?email=${user.email}`)
+                .then(res => res.json())
+                .then(data => setOrders(data))
+        }
+    }, [user])
     return (
         <div>
-            <h1>My Orders</h1>
+            <h2 className='font-bold text-purple-500 my-5 text-2xl'>My Orders: <span className='text-red-600'>{orders.length}</span></h2>
         </div>
     );
+
 };
 
-export default MyOrder;
+export default MyOrders;
