9feb6ac0a1b118ff5bddb24612bb8bfdf0c093da
Thowhid31 <thowhidantor16@gmail.com>
2022-05-26 08:14:56 +0600

    added stripe to payment

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,7 +1,11 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../../Components/Loading';
+import CheckoutForm from './CheckoutForm';
+const stripePromise = loadStripe('pk_test_51L0Us5A4HPuCHW2LKOm7foxYCz3Wqe3PJCM5YrIS6qIWs2ZjDQAE2wAJrnpqp8lJD09wft5X5JBnRDESSKh9Ib8w00usanoxlr');
 
 const Payment = () => {
 
@@ -22,25 +26,28 @@ const Payment = () => {
 
     return (
         <div className='flex justify-center items-center'>
-                    <div>
-                    <div class="card w-50 max-w-md bg-blue-200 shadow-xl my-10 text-black">
-                        <div class="card-body">
-                            <h1 className='text-blue-700 uppercase font-bold'>Hello, {parts.email}</h1>
-                            <h2 class="card-title">Pay for {parts.productName}</h2>
-                            <p>Your Delivery Done by us, within: 30 days.</p>
-                            <div class="card-actions justify-end">
-                                <p>Please Pay : ${parts.price}</p>
-                        
-                            </div>
+            <div>
+                <div class="card w-50 max-w-md bg-blue-200 shadow-xl my-10 text-black">
+                    <div class="card-body">
+                        <h1 className='text-blue-700 uppercase font-bold'>Hello, {parts.email}</h1>
+                        <h2 class="card-title">Pay for {parts.productName}</h2>
+                        <p>Your Delivery Done by us, within: 30 days.</p>
+                        <div class="card-actions justify-end">
+                            <p>Please Pay : ${parts.price}</p>
+
                         </div>
-                    </div>
-                    <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-blue-200 text-black">
-                        <div class="card-body">
-                        </div>
-                    </div>
                     </div>
                 </div>
-       
+                <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-blue-200 text-black">
+                    <div class="card-body">
+                        <Elements stripe={stripePromise}>
+                            <CheckoutForm />
+                        </Elements>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     );
 };
 
